1f735f50b398bbd72a537fd5c983b23d886969b0
Kauã <kaua.dalrover@gmail.com>
2025-09-07 21:16:34 -0300

    fix: use HashRouter to avoid 404

--- holerite-app-full/src/main.jsx
+++ holerite-app-full/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import App from "./App";
 import HoleriteApp from "./HoleriteApp";
 import LucroApp from "./LucroApp";
